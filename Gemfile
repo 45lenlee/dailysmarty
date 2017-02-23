848958ecef9b63e018efdcc9836405879c00d643
Len Lee <lenlee454@gmail.com>
2017-02-22 19:43:25 -0800

    create index and show view for Topic

--- Gemfile
+++ Gemfile
@@ -17,6 +17,7 @@ gem 'jquery-rails'
 gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.5'
 gem 'devise', '~> 4.2'
+gem 'friendly_id', '~> 5.2'
 
 
 group :development, :test do
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170222221525) do
+ActiveRecord::Schema.define(version: 20170223001557) do
+
+  create_table "topics", force: :cascade do |t|
+    t.string   "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string   "slug"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
